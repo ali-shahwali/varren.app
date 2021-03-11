60a6a76a5f981fc79138ff750ff4a65568d70c96
Ali Shahwali <ali_zinq@hotmail.com>
2021-03-11 06:53:07 +0100

    exams can now be listed

--- school/Data/ITentorRepositry.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-
-namespace school.Data {
-    public interface ITentorRepositry {
-        Task<IEnumerable<Tentor>> GetTentor();
-        Task<Tentor> GetTenta();
-    }
-}
