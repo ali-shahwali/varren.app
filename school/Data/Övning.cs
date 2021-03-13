aebcdc4ed7806581c6c680e5f8e8c4a1ef0fb38c
Ali Shahwali <ali_zinq@hotmail.com>
2021-03-13 12:16:13 +0100

    stuff

--- school/Data/Övning.cs
+++ school/Data/Övning.cs
@@ -10,6 +10,6 @@ namespace school.Data
         public int Id { get; set; }
         public string Kursnamn { get; set; }
         public string Ämne { get; set; }
-        public byte[] Data { get; set; }
+        public string Data { get; set; }
     }
 }
--- school/Migrations/20210313014020_Tentamodell5.Designer.cs
+++ school/Migrations/20210313014020_Tentamodell5.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using school.Data;
 
 namespace school.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210313014020_Tentamodell5")]
+    partial class Tentamodell5
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
