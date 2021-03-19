d14b918f171406b7e43ca7b0ae650f7e6f353e7e
Ali Shahwali <ali_zinq@hotmail.com>
2021-03-19 20:03:46 +0100

    website is now live at varren.app

---
+++ school/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using school.Data;
+
+[assembly: HostingStartup(typeof(school.Areas.Identity.IdentityHostingStartup))]
+namespace school.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
